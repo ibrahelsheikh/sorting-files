7ee7d3a609b43e016bef75ca6b80670fb4404a3b
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2023-01-18 16:41:54 +0200

    fix copied files to compressed file dir

--- copy_module/files.py
+++ copy_module/files.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def copy_files(src):
     # Define the source and destination directories for the photos
     src_dir = Path(src)
-    dst_dir = src_dir / 'compressed files'
+    dst_dir = src_dir / 'files'
 
     # Create the destination directory if it doesn't exist
     dst_dir.mkdir(exist_ok=True)
